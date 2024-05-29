47599f5d1bcd562a940cc6d3e61c06be4db2093d
Samir Caus <scaus@adobe.com>
2024-05-29 13:13:05 +0200

    update to only call getOffer

--- public/scripts/marketing.js
+++ public/scripts/marketing.js
@@ -4,44 +4,25 @@ const marketingOpsOnPageShow = (data) => {
   
   if (adobe) {
     console.log(JSON.stringify(data));
-    adobe.target.getOffers({
-        request: {
-          "prefetch": {
-            "views": [
-              {
-                "parameters": {
-                  "ad": "1"
-                },
-                "profileParameters": {
-                  "age": 23,
-                  "gender": "male",
-                  "categoryId": "shoe"
-                }
-              }
-            ]
-          }
-        }
-      });
+    adobe.target.getOffer({
+      "mbox": "target-global-mbox",
+      "params": {
+         "user.categoryId": "shoes",
+         "profile.age": 27,
+         "profile.gender": "male"
+      },
+      "success": function(offer) {
+            adobe.target.applyOffer( {
+               "mbox": "target-global-mbox",
+               "offer": offer
+            } );
+      },
+      "error": function(status, error) {
+          console.log('Error', status, error);
+      }
+    });
 
-    adobe.target.getOffers({
-        mbox: 'target-global-mbox',
-        request: {
-            execute: {
-                pageLoad: {
-                    "parameters": {
-                        "ad": "1"
-                      },
-                      "profileParameters": {
-                        "age": 23,
-                        "gender": "male",
-                        "categoryId": "shoe"
-                      }
-                }
-            }
-        }
-    }).then(response => adobe.target.applyOffers({ response: response }))
-        .then(() => console.log("Success getOffers applyOffers"))
-        .catch(error => console.log("Error", error));
+
 
   }
 }
