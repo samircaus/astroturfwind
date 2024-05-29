f347fc2d2a22f0000844001fabe4d348ef57c54c
Samir Caus <scaus@adobe.com>
2024-05-29 13:35:07 +0200

    add fake business logic

--- public/scripts/marketing.js
+++ public/scripts/marketing.js
@@ -1,29 +1,36 @@
 
 console.log("Custom Marketing ops scripts");
 const marketingOpsOnPageShow = (data) => {
-  
+
   if (adobe) {
     console.log(JSON.stringify(data));
+
+    let params = {}
+    if (window.location.href.indexOf('css') > 0) {
+      params = {
+        "user.categoryId": "shoes",
+        "profile.age": 27
+      }
+    } else if (window.location.href.indexOf('websites') > 0) {
+      params = {
+        "user.categoryId": "websites",
+        "profile.age": 27
+      }
+    }
+
     adobe.target.getOffer({
       "mbox": "target-global-mbox",
-      "params": {
-         "user.categoryId": "shoes",
-         "profile.age": 27,
-         "profile.gender": "male"
+      "params": params,
+      "success": function (offer) {
+        adobe.target.applyOffer( {
+           "mbox": "target-global-mbox",
+           "offer": offer
+        } );
       },
-      "success": function(offer) {
-            adobe.target.applyOffer( {
-               "mbox": "target-global-mbox",
-               "offer": offer
-            } );
-      },
-      "error": function(status, error) {
-          console.log('Error', status, error);
+      "error": function (status, error) {
+        console.log('Error', status, error);
       }
     });
-
-
-
   }
+
 }
- 
